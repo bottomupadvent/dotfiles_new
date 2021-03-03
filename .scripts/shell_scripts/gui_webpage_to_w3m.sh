742bee4027c6f85c3c9b775d8cd831288352bd12
Instincts9o2 <pranavchaudhari97@gmail.com>
2021-03-03 13:35:32 +0530

    Added gui_webpage_to_w3m.sh file added

---
+++ .scripts/shell_scripts/gui_webpage_to_w3m.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script is mapped to a keybinding Ctrl+alt+w in ~/.xbindkeysrc
+# Copies current tab URL in google-chrome and open it in w3m
+# Script requires xdotool, tmux, google-chrome, xsel
+
+xdotool search --name google-chrome key ctrl+l ctrl+c
+tmux run-shell "xsel -op > /tmp/webpage_url.chrome" ; tmux load-buffer /tmp/webpage_url.chrome
+tmux send-keys -t web:0.1 t o C-u ; tmux paste-buffer -t web:0.1 ; tmux send-keys -t web:0.1 Enter
+tmux switch-client -t web:0.1
+xdotool key alt+Tab
