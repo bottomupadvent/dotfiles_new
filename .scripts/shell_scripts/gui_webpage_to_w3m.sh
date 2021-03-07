ac1e219ad569716273448a7670d10ea7a0419666
Instincts9o2 <pranavchaudhari97@gmail.com>
2021-03-07 20:57:00 +0530

    Minor Changes

--- .scripts/shell_scripts/gui_webpage_to_w3m.sh
+++ .scripts/shell_scripts/gui_webpage_to_w3m.sh
@@ -3,7 +3,7 @@
 # Copies current tab URL in google-chrome and open it in w3m
 # Script requires xdotool, tmux, google-chrome, xsel
 
-xdotool search --name google-chrome key ctrl+l ctrl+c
+xdotool search --name "google-chrome" key ctrl+l ctrl+c
 tmux run-shell "xsel -op > /tmp/webpage_url.chrome" ; tmux load-buffer /tmp/webpage_url.chrome
 tmux send-keys -t web:0.1 t o C-u ; tmux paste-buffer -t web:0.1 ; tmux send-keys -t web:0.1 Enter
 tmux switch-client -t web:0.1
