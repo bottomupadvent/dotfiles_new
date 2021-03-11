9a124374c25cd8a7e855d8c45f8ce343c1e27d01
Instincts9o2 <pranavchaudhari97@gmail.com>
2021-03-11 12:57:08 +0530

    Minor Changes

--- .scripts/shell_scripts/gui_webpage_to_w3m.sh
+++ .scripts/shell_scripts/gui_webpage_to_w3m.sh
@@ -4,4 +4,6 @@ xdotool search "Mozilla Firefox" windowactivate --sync key --clearmodifiers ctrl
 tmux run-shell "xsel -op > /tmp/webpage_url.chrome" ; tmux load-buffer /tmp/webpage_url.chrome
 tmux switch-client -t web:0.1
 tmux send-keys -t web:0.1 t o C-u ; tmux paste-buffer -t web:0.1 ; tmux send-keys -t web:0.1 Enter
-xdotool key --clearmodifiers alt+Tab
+
+konsole_winID=$(xdotool search --name "~ : tmux: client")
+xdotool windowactivate $konsole_winID
