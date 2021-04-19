3998278188123cb51c75eec3e05a0181ac18766e
Instincts9o2 <pranavchaudhari97@gmail.com>
2021-04-19 20:25:19 +0530

    Minor Changes

--- Source/st/config.h
+++ Source/st/config.h
@@ -7,7 +7,7 @@
  */
 static char *font = "Roboto Mono for Powerline:pixelsize=14:antialias=true:autohint=true";
 
-static int borderpx = 2;
+static int borderpx = 20;
 
 /*
  * What program is execed by st depends of these precedence rules:
